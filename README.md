b6b083932db5817167f46c87abc433fc8f9808b4
Gabe Davis <gabedavis001@yahoo.com.au>
2022-08-07 15:16:14 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # youtube_downloader
+
 Download YouTube videos and convert to mp3 audio
+
+Python code to download YouTube videos by URL, grab the mp4 then convert to mp3.
+
+Note: I do not support copyright infringement, the code is not to be ued for illegally obtaining copyrighted content.  
---
+++ youtube_down_converter.py
@@ -0,0 +1,30 @@
+import os
+from moviepy.editor import *
+import pytube
+
+''' Script to download YouTube videoa and convert to .mp3'''
+
+
+def convert_to_mp3(video, name):
+    video = VideoFileClip(os.path.join(video))
+    name = video.audio.write_audiofile(os.path.join(name))
+
+def get_vid(link):
+    save_path = "."
+    try:
+        yt = pytube.YouTube(link)
+    except: 
+        print("Connection Error, unable to reach YouTube")
+    title = yt.streams[0].title
+    video_name = title + ".mp4"
+    stream = yt.streams.first()
+    try:
+        stream.download(save_path)
+    except: 
+        print("Unable to download the YouTube video") 
+    os.rename(yt.streams.first().default_filename, video_name)
+    convert_to_mp3(video_name, title + ".mp3")
+    os.remove(video_name)
+
+get_vid("https://www.youtube.com/watch?v=o9A-w11--8o")
+
