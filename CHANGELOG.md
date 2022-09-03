fd48be98436b41bf4d4c76557ff76e0fdc28c03d
Gabe Davis <gabedavis001@yahoo.com.au>
2022-09-03 19:23:37 +1000

    Added requirements.txt

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# YouTube Processor Changelog
+
+0.0.2:  Added class structure
+        improved type hinting  
+0.0.1:  Initial version 
+
--- youtube_processor.py
+++ youtube_processor.py
@@ -2,24 +2,27 @@
 
 import argparse
 import json
-from mutagen.mp3 import MP3
 from moviepy.editor import *
+from mutagen.mp3 import MP3
 import os
 import pytube
 from youtube_transcript_api import YouTubeTranscriptApi
 
 '''
 Code to download YouTube video, extract audio and convert to mp3.  After that go back to YouTube 
-and get transcript. Then format into a more readbale version of JSON. 
+and get transcript. Then format into a more readbale version of JSON and provide cumulative duration
+of mp3 audio in the nominated mp3 directory. 
 
-Version: 0.0.1
+Author: G3Dx5
+Version: 0.0.2
 
 TODO
 
 [-] Make record of files 
-[-] Add error handling
+[-] Add better error handling
+[-] Improve JSON formatting 
+Usage:  python3 youtube_processor -l "https://youtube.com/your_yt_video"
 
-Usage:  python3 youtube_processor -i "https://youtube.com/your_yt_video"
 '''
 
 ''' Process input arguments '''
@@ -27,95 +30,107 @@ parser = argparse.ArgumentParser(description='YouTube Dowloader, extractor and t
 parser.add_argument("-l", "--YouTubeLink", help="YouTube Link")
 args = parser.parse_args()
 
-''' Dictionary for link, ID and output JSON file '''
-video_data = {"YouTubeLink":[args.YouTubeLink],"Name":[],"ID":[], "JSONFile":[]}
+class Youtubeprocessor:
+    ''' Dictionary for link, ID and output JSON file '''    
+    video_data = {"YouTubeLink":[args.YouTubeLink],"Name":[],"ID":[], "JSONFile":[]}
+    
+    print("Querying YouTube....")
+
+    @classmethod
+    def getYTvideoName(cls) -> str:
+        ''' Get YouTube Video name for naming the output files '''
+        link: str = str(cls.video_data["YouTubeLink"])
+        video_name: str = pytube.YouTube(link)
+        title: str = video_name.streams[0].title
+        cls.video_data["Name"] = title
+
+    @classmethod
+    def seperateYtID(cls) -> str:
+        ''' Extract ID from the YouTube link and populate the dictionary '''
+        raw_ID = str(cls.video_data["YouTubeLink"])
+        ID = raw_ID.split("=")[1].split("&")[0].replace("'","").replace("]","")
+        cls.video_data["ID"] = ID
+    
+    def convert_to_mp3(video, name) -> None:
+        ''' convert YT video to mp3 '''
+        video = VideoFileClip(os.path.join(video))
+        name = video.audio.write_audiofile(os.path.join(name))
+
+    @classmethod
+    def getYouTubeVideo(cls,link) -> None:
+        save_path = "."
+        try:
+            video = pytube.YouTube(str(link))
+        except: 
+            print("Connection Error, unable to reach YouTube")
+        video_name = str(cls.video_data["Name"]) + ".mp4"
+        stream = video.streams.first()
+        try:
+            stream.download(save_path)
+        except: 
+            print("Unable to download YouTube video") 
+        os.rename(video.streams.first().default_filename, video_name)
+        cls.convert_to_mp3(video_name, cls.video_data["Name"] + ".mp3")
+
+    @classmethod
+    def getTranscript(cls) -> None:
+        ''' Get YouTube transcript '''
+        ytID = cls.video_data["ID"]
+        transcript = YouTubeTranscriptApi.get_transcript(ytID)
+        JSON_name = cls.video_data["Name"]
+        JSONFile = JSON_name + ".json"
+        cls.video_data["JSONFile"] = JSONFile
+        with open(JSONFile, 'w', encoding='utf-8') as json_file:
+            json.dump(transcript, json_file)
+
+    @classmethod
+    def processJSON(cls):
+        ''' Take JSON transcript from YouTube and reformat '''
+        JSON_string = None
+        with open(cls.video_data["JSONFile"]) as f:
+            JSON_string = f.read()
+        try:
+            parsed_json = json.loads(JSON_string)
+            formatted_json = json.dumps(parsed_json, indent = 4,sort_keys=True)
+            with open(cls.video_data["Name"] + "_formatted" + ".json", "w") as tf:
+                tf.write(formatted_json)
+        except Exception as e:
+            print(repr(e))
 
 
-def getYTvideoName() -> str:
-    ''' Get YouTube Video name for naming the output files '''
-    link = str(video_data["YouTubeLink"])
-    video_name = pytube.YouTube(link)
-    title = video_name.streams[0].title
-    video_data["Name"] = title
+    def convertSeconds(seconds):
+        ''' convert seconds to minute / hour units'''
+        hours = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+        return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
-def seperateYtID() -> str:
-    ''' Extract ID from the YouTube link and populate the dictionary '''
-    raw_ID = str(video_data["YouTubeLink"])
-    ID = raw_ID.split("=")[1].split("&")[0].replace("'","").replace("]","")
-    video_data["ID"] = ID
-
-def convert_to_mp3(video, name) -> None:
-    ''' convert YT video to mp3 '''
-    video = VideoFileClip(os.path.join(video))
-    name = video.audio.write_audiofile(os.path.join(name))
-
-def getYouTubeVideo(link) -> None:
-    save_path = "."
-    try:
-        video = pytube.YouTube(str(link))
-    except: 
-        print("Connection Error, unable to reach YouTube")
-    video_name = str(video_data["Name"]) + ".mp4"
-    stream = video.streams.first()
-    try:
-        stream.download(save_path)
-    except: 
-        print("Unable to download YouTube video") 
-    os.rename(video.streams.first().default_filename, video_name)
-    convert_to_mp3(video_name, video_data["Name"] + ".mp3")
-
-def getTranscript() -> None:
-    ''' Get YouTube transcript '''
-    ytID = video_data["ID"]
-    transcript = YouTubeTranscriptApi.get_transcript(ytID)
-    JSON_name = video_data["Name"]
-    JSONFile = JSON_name + ".json"
-    video_data["JSONFile"] = JSONFile
-    with open(JSONFile, 'w', encoding='utf-8') as json_file:
-        json.dump(transcript, json_file)
-
-def processJSON():
-    ''' Take JSON transcript from YouTube and reformat '''
-    JSON_string = None
-    with open(video_data["JSONFile"]) as f:
-        JSON_string = f.read()
-    try:
-        parsed_json = json.loads(JSON_string)
-        formatted_json = json.dumps(parsed_json, indent = 4,sort_keys=True)
-        with open(video_data["Name"] + "_formatted" + ".json", "w") as tf:
-            tf.write(formatted_json)
-    except Exception as e:
-        print(repr(e))
-
-def convertSeconds(seconds):
-    ''' convert seconds to minute / hour units'''
-    hours = seconds // 3600
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60
-    return "%02d:%02d:%02d" % (hours, minutes, seconds)
-
-def getDuration(path):
-    ''' get mp3 video length '''
-    tot_length = 0
-    for root, dir, files in os.walk(os.path.abspath(path)):
-        for file in files:
-            if file.endswith(".mp3"):
-                audio = MP3(os.path.join(root, file))
-                length = audio.info.length
-                tot_length += length
-        hours, minutes, seconds = convertSeconds(tot_length).split(":")
-        print("Duration: " + str(int(hours)) + ':' + str(int(minutes)) + ':' + str(int(seconds)))
+    @classmethod
+    def getDuration(cls, path):
+        ''' get mp3 video length '''
+        tot_length = 0
+        for root, dir, files in os.walk(os.path.abspath(path)):
+            for file in files:
+                if file.endswith(".mp3"):
+                    audio = MP3(os.path.join(root, file))
+                    length = audio.info.length
+                    tot_length += length
+            hours, minutes, seconds = cls.convertSeconds(tot_length).split(":")
+        print("Total Duration in folder: " + str(int(hours)) + ':' + str(int(minutes)) + ':' + str(int(seconds)))
 
             
 def main():
-    getYTvideoName()
-    seperateYtID()
-    getYouTubeVideo(video_data["YouTubeLink"])
-    getTranscript()
-    processJSON()
-    getDuration("/location/of/your/mp3files")
+    Youtubeprocessor.getYTvideoName()
+    Youtubeprocessor.seperateYtID()
+    Youtubeprocessor.getYouTubeVideo(args.YouTubeLink)
+    Youtubeprocessor.getTranscript()
+    Youtubeprocessor.processJSON()
+    #Youtubeprocessor.getDuration(".")
     
 if __name__== "__main__" :
     main()
+
+
+
 
